2cae5ffcfc2878a96ed278e19e8b8a926c34a620
anton <friendlyantz@pm.me>
2024-04-15 23:50:45 +1000

    Solve graphemes

--- reverse-string/src/lib.rs
+++ reverse-string/src/lib.rs
@@ -1,4 +1,13 @@
+use unicode_segmentation::UnicodeSegmentation;
+
 pub fn reverse(input: &str) -> String {
-    let reversed: String = input.chars().rev().collect();
+    let graphemes = UnicodeSegmentation::graphemes(input, true)
+        .collect::<Vec<&str>>();
+    // let reversed: String = input.chars().rev().collect();
+    let reversed: String = graphemes
+        .into_iter()
+        .rev()
+        .collect();
+
     reversed
 }
